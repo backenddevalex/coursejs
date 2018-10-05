63a861bd33633f837cc19c397d3c812ac46a5448
Aleksandr Kuznetsov <legioner198_11@mail.ru>
2018-10-05 21:30:46 +1000

    Второй коммит дополнение

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let money = prompt("Ващ бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+let money = prompt("Ващ бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 
 let appData = {
@@ -17,13 +17,15 @@ let appData = {
 
 // Скрипт создал переменные для общение с пользователем и занес некоторые данные в внутренние свойства обьекта
 
-let coins = prompt('Введите обязательную статью расходов в этом месяце');
-let expense = prompt('Во сколько обойдётся?');
-let coin = prompt('Введите обязательную статью расходов в этом месяце');
-let epx = prompt('Во сколько обойдётся?');
+let coinsOne = prompt('Введите обязательную статью расходов в этом месяце', '');
+let expenseOne = prompt('Во сколько обойдётся?', '');
+let coinsTwo = prompt('Введите обязательную статью расходов в этом месяце', '');
+let expenseTwo= prompt('Во сколько обойдётся?', '');
 
 
-appData.expenses[coins, coin]=expense, epx;
+appData.expenses[coinsOne]=expenseOne;
+appData.expense[coinsTwo] = expenseTwo;
+console.log(appData.expenses);
 
 // Созданные переменные в итоге были добавлены внутренний свойством в внутренний обьект expense
 
