95d6db50ad679b54738653581e515f68104eae7d
Aleksandr Kuznetsov <legioner198_11@mail.ru>
2018-10-08 23:19:05 +1000

    Третий коммит правки

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -5,12 +5,12 @@ let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 
 let appData = {
-  coins: money,
-  timeData: time,
-  expenses: {},
-  optionalExpenses: {},
-  income: [],
-  savings: false
+    coins: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
 
 // Скрипт создал переменные для общение с пользователем и занес некоторые данные в внутренние свойства обьекта
@@ -26,50 +26,63 @@ let appData = {
 
 
 for (let i = 0; i < 2; i++) {
-  let coinsOne = prompt('Введите обязательную статью расходов в этом месяце', '');
-  let expenseOne = prompt('Во сколько обойдётся?', '');
+      let coinsOne = prompt('Введите обязательную статью расходов в этом месяце', '');
+      let expenseOne = prompt('Во сколько обойдётся?', '');
+        if ( (typeof(coinsOne))=== "string" && coinsOne != null && expenseOne != null
+      && coinsOne != '' && expenseOne != '' && coinsOne.length <50) {
+        console.log('done')
+        appData.expenses.coinsOne = expenseOne;
+      } else {
+         i--;
+        alert("Данные неверны, пожалуйста введите снова");
+      }
+    };
+
+    appData.moneyPerDay = money / 30;
+
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+    if(appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка")
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка")
+    } else if (appData.moneyPerDay < 2000) {
+        console.log("Высокий  уровень достатка")
+    } else {
+        console.log("Ошибка расчета")
+    }
 
 
-  if ( (typeof(coinsOne))=== "string" && coinsOne != null && expenseOne != null
-  && coinsOne != '' && expenseOne != '' && coinsOne.length <50) {
-    console.log('done')
-    appData.expenses.coinsOne = expenseOne;
-  } else {
-     i--;
-    alert("Данные неверны, пожалуйста введите снова");
-  }
-};
-
-appData.moneyPerDay = money / 30;
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка")
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уровень достатка")
-} else if (appData.moneyPerDay < 2000) {
-    console.log("Высокий  уровень достатка")
-} else {
-    console.log("Ошибка расчета")
-}
+    // while (i < 2) {
+    //     let coinsOne = prompt('Введите обязательную статью расходов в этом месяце', '');
+    //     let expenseOne = prompt('Во сколько обойдётся?', '');
+    // if ((typeof(coinsOne))=== "string" && coinsOne != null && expenseOne != null
+    //   && coinsOne != '' && expenseOne != '' && coinsOne.length <50) {
+    //     console.log("Правильно")
+    //     appData.expenses[coinsOne] = expenseOne;
+    //   } else {
+    //       i--;  
+    //    alert("Неправильно");     
+    //   }
+    //   i++;
+    // } 
 
 
-// let a = 2;
 // do {
-//     let coinsOne = prompt('Введите обязательную статью расходов в этом месяце', '');
-//     let expenseOne = +prompt('Во сколько обойдётся?', '');
-//     console.log("Правильно")
-//     appData.expenses[coinsOne] = expenseOne;
-//     a++;
-// } while (a < 2);
+    //     let coinsOne = prompt('Введите обязательную статью расходов в этом месяце', '');
+    //     let expenseOne = prompt('Во сколько обойдётся?', '');
+    //     if ((typeof (coinsOne)) === "string" && coinsOne != null && expenseOne != null &&
+    //         coinsOne != '' && expenseOne != '' && coinsOne.length < 50) {
+    //         console.log("Правильно")
+    //         appData.expenses[coinsOne] = expenseOne;
+    //     } else {
+    //         i--;
+    //         alert("Данные неверны, пожалуйста введите снова");
+    //     }
+    // }
+    // while (i > 2);
 
 
 
-// while (a < 2) {
-//     let coinsOne = prompt('Введите обязательную статью расходов в этом месяце', '');
-//     let expenseOne = +prompt('Во сколько обойдётся?', '');
-//     console.log("Правильно")
-//     appData.expenses[coinsOne] = expenseOne;
-//     a++;
-// };
+
+
