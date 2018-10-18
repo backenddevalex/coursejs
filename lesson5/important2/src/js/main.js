27f17b71c4101a00a06b200f6588d87164e1b969
Aleksandr Kuznetsov <legioner198_11@mail.ru>
2018-10-18 19:09:37 +1000

    Пятый урок

---
+++ lesson5/important2/src/js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let beginPay = document.getElementById ("start"),
+    inpExp = document.getElementsByClassName('expenses-item'),
+    btnOne = document.getElementsByTagName('button.expenses-item-btn'),
+    btnTwo = document.getElementsByTagName('button.count-budget-btn'),
+    optExp = document.querySelectorAll(".optionalexpenses-item"),
+    possInc = document.querySelector(".choose-income"),
+    checkBox = document.querySelector("#savings"),
+    summ = document.querySelector(".choose-sum"),
+    perc = document.querySelector(".choose-percent"),
+    year = document.querySelector(".year-value"),
+    month = document.querySelector(".month-value"),
+    times = document.querySelector(".day-value");
+    console.log(btnOne, btnTwo);
+
+
+
+    let value = [];
+  value[0] = document.getElementsByClassName('budget-value'),
+  value[1] = document.getElementsByClassName("daybudget-value"),
+  value[2] = document.getElementsByClassName('level-value'),
+  value[3] = document.getElementsByClassName('expenses-value'),
+  value[4] = document.getElementsByClassName('optionalexpenses-value'),
+  value[5] = document.getElementsByClassName('income-value'),
+  value[6] = document.getElementsByClassName('monthsavings-value'),
+  value[7] = document.getElementsByClassName('yearsavings-value');
+
+
+
